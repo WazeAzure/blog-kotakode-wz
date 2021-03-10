ea809aff30f9d8f0a0a5a0d1de7b5d5a2de647b4
WazeAzure <bryan.rich0604@gmail.com>
2021-03-10 10:25:02 +0800

    adding z-index to del button

--- simple-crud-js-localstorage/script.js
+++ simple-crud-js-localstorage/script.js
@@ -42,7 +42,7 @@ function show(notes){
         test.setAttribute('draggable','true')
 		test.setAttribute('class','list-group-item d-flex justify-content-between align-items-center')
         test.setAttribute('id','listing')
-		test.innerHTML = `${notes[i].val} <button class="btn btn-outline-danger btn-sm" onclick="del(${i})">delete</button>`
+		test.innerHTML = `${notes[i].val} <button class="btn btn-outline-danger btn-sm" style="z-index:10" onclick="del(${i})">delete</button>`
 
 		list.appendChild(test)
 	}
